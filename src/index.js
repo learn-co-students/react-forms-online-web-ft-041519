2d6a90d42ac725caeb807b38e4fbc47061822adb
Kish-Siva-1 <rodes88@msn.com>
2019-09-02 22:39:05 +0200

    Done.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'
+import Form from './components/ControlledInput';
 
 ReactDOM.render(
-  <div>Mount Components Here</div>,
+  <div><Form prop1='random' /></div>,
   document.getElementById('root')
 );
